a75bc57a8e4babb2fff5805956aa3e180ddbcbd5
tea <teodora.pekovic@netcast.rs>
2022-06-05 01:43:08 +0200

    cart updated

--- src/components/CartMenu.jsx
+++ src/components/CartMenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import {
   Button,
   Box,
@@ -9,16 +9,19 @@ import {
   ListItemText,
   Typography,
   Divider,
+  Grid,
 } from "@material-ui/core";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import DraftsIcon from "@material-ui/icons/Drafts";
 import SendIcon from "@material-ui/icons/Send";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import CloseIcon from "@material-ui/icons/Close";
+import placeholderPhotoCart from "../assets/img/default-photo.jpg";
 
 const StyledMenu = withStyles({
   paper: {
     border: "1px solid #d3d4d5",
+    width: "25em",
   },
 })((props) => (
   <Menu
@@ -39,7 +42,7 @@ const StyledMenu = withStyles({
 const StyledMenuItem = withStyles((theme) => ({
   root: {
     "&:focus": {
-      backgroundColor: theme.palette.primary.main,
+      backgroundColor: theme.palette.common.white,
       "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
         color: theme.palette.common.white,
       },
@@ -47,7 +50,39 @@ const StyledMenuItem = withStyles((theme) => ({
   },
 }))(MenuItem);
 
+const useStyles = makeStyles((theme) => ({
+  subtotal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  placeholderPhotoCart: {
+    maxWidth: "100px",
+    maxHeight: "100px",
+    objectFit: "fill",
+  },
+  cartMenuBottom: {
+    padding: "1.25em",
+  },
+  cartCheckoutButton: {
+    width: "100%",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "start",
+    width: "100%",
+  },
+  price: {
+    display: "flex",
+    justifyContent: "end",
+    width: "100%",
+  },
+}));
+
 const CartMenu = () => {
+  const classes = useStyles();
+
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -59,7 +94,7 @@ const CartMenu = () => {
   };
 
   return (
-    <div>
+    <Box>
       <Button
         aria-controls="customized-menu"
         aria-haspopup="true"
@@ -74,22 +109,34 @@ const CartMenu = () => {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <Box>
-          <Typography variant="h6">Your cart</Typography>
-          <Button>
-            <CloseIcon />
+        <StyledMenuItem>
+          <img
+            className={classes.placeholderPhotoCart}
+            src={placeholderPhotoCart}
+          />
+          <Box className={classes.title}>
+            <Typography variant="h6">Title placeholder</Typography>
+          </Box>
+          <Box className={classes.price}>
+            <Typography>Price: $</Typography>
+          </Box>
+        </StyledMenuItem>
+        <Divider />
+        <Box className={classes.cartMenuBottom}>
+          <Box className={classes.subtotal}>
+            <Typography variant="h6">Subtotal: </Typography>
+            <Typography variant="h6">$</Typography>
+          </Box>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.cartCheckoutButton}
+          >
+            Checkout
           </Button>
         </Box>
-        <Divider />
-
-        <StyledMenuItem>
-          <ListItemIcon>
-            <SendIcon fontSize="small" />
-          </ListItemIcon>
-          <ListItemText primary="Sent mail" />
-        </StyledMenuItem>
       </StyledMenu>
-    </div>
+    </Box>
   );
 };
 
